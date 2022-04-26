328bcaf8d5477cc41997e2d3e8440aafa2d8dca6
Rohan Mishra <rohan.mishra1997@gmail.com>
2022-04-26 11:07:12 +0200

    typo

--- src/endpoints.py
+++ src/endpoints.py
@@ -16,10 +16,10 @@ class CategoricPayload(BaseModel):
 class CategoricResponse(BaseModel):
     result: str
 
-@client.endpoint(X=NumericPayload, y=NumericResponse)
-def numeric_endpoint(payload: NumericPayload, track_inputs=["feature"], track_outputs=["result"]):
+@client.endpoint(X=NumericPayload, y=NumericResponse, track_inputs=["feature"], track_outputs=["result"])
+def numeric_endpoint(payload: NumericPayload):
     return NumericResponse(result=payload.feature)
 
-@client.endpoint(X=CategoricPayload, y=CategoricResponse)
-def categoric_endpoint(payload: CategoricPayload, track_inputs=["feature"], track_outputs=["result"]):
+@client.endpoint(X=CategoricPayload, y=CategoricResponse, track_inputs=["feature"], track_outputs=["result"])
+def categoric_endpoint(payload: CategoricPayload):
     return CategoricResponse(result=payload.feature)
