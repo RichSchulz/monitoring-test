8f8a72c46c4fe8ce4fb96063c65b42750427a25b
Richard Schulz <49609334+RichSchulz@users.noreply.github.com>
2022-07-20 09:42:20 +0200

    Update endpoints.py

--- src/endpoints.py
+++ src/endpoints.py
@@ -10,7 +10,7 @@ class NumericPayload(BaseModel):
     this_is_an_even_longer_variable_name_similar_to_branch_1: int
     this_is_an_even_longer_variable_name_similar_to_branch_2: int
     this_is_an_even_longer_variable_name_similar_to_branch_3: int
-    user.events.timing_0.6__credit_score_clicked_app_rating_dialog_seconds_total: int
+    user.events.timing_0.6_credit_score_clicked_app_rating_dialog_seconds_total: int
 
 
 class NumericResponse(BaseModel):
@@ -22,7 +22,7 @@ class CategoricPayload(BaseModel):
 class CategoricResponse(BaseModel):
     result: str
 
-@client.endpoint(X=NumericPayload, y=NumericResponse, track_inputs=["feature", "this_is_a_super_long_variable_name_similar_to_branch", "this_is_an_even_longer_variable_name_similar_to_branch_1", "this_is_an_even_longer_variable_name_similar_to_branch_2", "this_is_an_even_longer_variable_name_similar_to_branch_3", "user.events.timing_0.6__credit_score_clicked_app_rating_dialog_seconds_total"], track_outputs=["result"])
+@client.endpoint(X=NumericPayload, y=NumericResponse, track_inputs=["feature", "this_is_a_super_long_variable_name_similar_to_branch", "this_is_an_even_longer_variable_name_similar_to_branch_1", "this_is_an_even_longer_variable_name_similar_to_branch_2", "this_is_an_even_longer_variable_name_similar_to_branch_3", "user.events.timing_0.6_credit_score_clicked_app_rating_dialog_seconds_total"], track_outputs=["result"])
 def numeric_endpoint(payload: NumericPayload):
     return NumericResponse(result=payload.feature)
 
